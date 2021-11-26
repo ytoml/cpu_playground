6eedc281613abc88b3c1aaf9af99927755f41913
Yuna Tomida <ytomida.mmm@gmail.com>
2021-11-26 15:35:45 +0900

    test.sv を修正

--- harris/single/test/test.sv
+++ harris/single/test/test.sv
@@ -6,7 +6,19 @@ module test();
 	initial begin
 		clk <= 0; reset <= 1;
 		#10 reset <= 0;
+		#1000
+	end
 
+	always @(negedge clk) begin
+		if (write_enab) begin
+			if (data_addr === 84 && write_data === 7) begin 
+				$display("Simulation Succeeded.");
+				$stop;
+			end else if (data_addr !== 80) begin 
+				$display("Simulation Failed.");
+				$stop;
+			end
+		end
 	end
 
 	always #5 clk <= ~clk;
