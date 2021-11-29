072325506001011918abc8b5746a37cd558bff82
Yuna Tomida <ytomida.mmm@gmail.com>
2021-11-29 20:40:17 +0900

    mux4 の記述、datapath の追記(未完成)

--- harris/multi/main/mother_board/cpu/datapath.sv
+++ harris/multi/main/mother_board/cpu/datapath.sv
@@ -6,20 +6,23 @@ module datapath(
 	output	FUNCT		funct,
     output  logic       zero,
     input   logic       mem_to_reg,
-    input   logic       pc_src, alu_src,
+    input   logic       pc_src, alu_srcA,
+    input   logic[1:0]	alu_srcB,
     input   logic       reg_dst, reg_write,
     input   logic       jmp,
 	input	logic		ireg_write_enab,
+	input	logic		pc_write_enab,
 	input	logic		i_or_d,
     input   logic[2:0]  alu_ctrl_sig,
     output  logic[31:0] write_data
 );
-    logic[31:0] alu_out, inst, imm, reg_in, read_data, load_data;
+    logic[31:0] alu_out, inst, imm, reg_in, read_data;
     logic[31:0] pc, pc_plus4, pc_next, pc_br, pc_br_next, br_offset, pc_jmp;
-    logic[31:0] alu_in1, alu_in2;
+    logic[31:0] rs_out, rs_to_A, rt_out, alu_inA, alu_inB, alu_res;
     logic[4:0]  reg_id;
 
-	mux2 #(.N(32))	sel_addr(.sel(i_or_d), .src1(pc), .src2(alu_out), .out(mem_bus.addr))
+	// メモリのアドレス選択
+	mux2 #(.N(32))	sel_addr(.sel(i_or_d), .src0(pc), .src1(alu_out), .out(mem_bus.addr))
 
 	// メモリから読んできたデータを命令/データレジスタに格納
 	enab_ff #(.N(32))	ireg(.ctrl_bus, .enab(ireg_write_enab), .in(mem_bus.data), .out(inst))
@@ -32,26 +35,30 @@ module datapath(
     );
 
 	// PC 制御
-	ff #(.N(32))	pcreg(.ctrl_bus, .in(pc_next), .out(pc));
-	assign pc_plus4		= pc + 32'b100;
+	enab_ff #(.N(32))	pcreg(.ctrl_bus, .enab(pc_write_enab), .in(pc_next), .out(pc));
 	assign br_offset	= { imm[29:0], 2'b00 };
 	assign pc_br		= pc_plus4 + br_offset;
 	assign pc_jmp		= { pc_plus4[31:28], inst[25:0], 2'b00 }; // word(4byte) alignment
 
 	// beq は 次の命令(pc_plus4) からの相対でアドレシング、j は(擬似)直接アドレシング
-    mux2 #(.N(32))  pc_br_select(.sel(pc_src), .src1(pc_plus4), .src2(pc_br), .out(pc_br_next));
-	mux2 #(.N(32))	pc_select(.sel(jmp), .src1(pc_br_next), .src2(pc_jmp), .out(pc_next));
+    mux2 #(.N(32))  pc_br_select(.sel(pc_src), .src0(pc_plus4), .src1(pc_br), .out(pc_br_next));
+	mux2 #(.N(32))	pc_select(.sel(jmp), .src0(pc_br_next), .src1(pc_jmp), .out(pc_next));
 
     // lw 命令では inst[20:16], R 形式では inst[15:11] をディスティネーションに設定
-    mux2 #(.N(5))  sel_dst(.sel(reg_dst), .src1(inst[20:16]), .src2(inst[15:11]), .out(reg_id));
+    mux2 #(.N(5))  sel_dst(.sel(reg_dst), .src0(inst[20:16]), .src1(inst[15:11]), .out(reg_id));
     regfile         regfile(
         .ctrl_bus, .reg_write,
         .rs(inst[25:21]), .rt(inst[20:16]), .rd(reg_id),
-        .reg_in, .rs_data(alu_in1), .rt_data(write_data)
+        .reg_in, .rs_data(rs_out), .rt_data(rt_out)
     );
+	ff #(.N(32))	rs_buf(.ctrl_bus, .in(rs_out), .out(rs_to_A));
+	ff #(.N(32))	rt_buf(.ctrl_bus, .in(rt_out), .out(write_data));
+
     sign_ext        sign_ext(.half(inst[15:0]), .full(imm));
-    mux2 #(.N(32))  res_to_reg(.sel(mem_to_reg), .src1(alu_out), .src2(read_data), .out(reg_in));
+    mux2 #(.N(32))  res_to_reg(.sel(mem_to_reg), .src0(alu_out), .src1(read_data), .out(reg_in));
     
-    mux2 #(.N(32))  alu_src_sel(.sel(alu_src), .src1(write_data), .src2(imm), .out(alu_in2));
-    alu #(.N(32))   alu(.src1(alu_in1), .src2(alu_in2), .alu_ctrl_sig, .alu_out, .zero);
+    mux2 #(.N(32))  alu_srcA_sel(.sel(alu_srcA), .src0(pc), .src1(imm), .out(alu_inA));
+	mux4 #(.N(32))	alu_srcB_sel(.sel(alu_srcB), .src1(32'b100), .src2(imm), .src3(br_offset), .out(alu_inB));
+    alu #(.N(32))   alu(.src0(alu_inA), .src1(alu_inB), .alu_ctrl_sig, .alu_res, .zero);
+    ff	#(.N(32))	alu_buf(.ctrl_bus, .in(alu_res), .out(alu_out));
 endmodule
--- harris/multi/main/mother_board/cpu/datapath/alu.sv
+++ harris/multi/main/mother_board/cpu/datapath/alu.sv
@@ -1,7 +1,7 @@
 module alu #(parameter N)(
     input   logic[N-1:0] src1, src2,
     input   logic[2:0]  alu_ctrl_sig,
-    output  logic[N-1:0] alu_out,
+    output  logic[N-1:0] alu_res,
     output  logic       zero
 );
 
@@ -10,17 +10,17 @@ module alu #(parameter N)(
     // MIPS の ALU にはオーバーフローフラグがない
     always_comb begin
         unique case (alu_ctrl_sig)
-            3'b000: alu_out = src1 & src2;
-            3'b001: alu_out = src1 | src2;
-            3'b010: alu_out = src1 + src2;
-            3'b011: alu_out = 32'bx;                    // 使用しない
-            3'b100: alu_out = src1 & ~src2;
-            3'b101: alu_out = src1 | ~src2;
-            3'b110: alu_out = sub;
-            3'b111: alu_out = {{N-1{1'b0}}, sub[N-1]};   // set less than
-            default: alu_out = 32'bx;
+            3'b000: alu_res = src1 & src2;
+            3'b001: alu_res = src1 | src2;
+            3'b010: alu_res = src1 + src2;
+            3'b011: alu_res = 32'bx;                    // 使用しない
+            3'b100: alu_res = src1 & ~src2;
+            3'b101: alu_res = src1 | ~src2;
+            3'b110: alu_res = sub;
+            3'b111: alu_res = {{N-1{1'b0}}, sub[N-1]};   // set less than
+            default: alu_res = 32'bx;
         endcase
     end
-    assign zero = alu_out == 0;
+    assign zero = alu_res == 0;
     
 endmodule
