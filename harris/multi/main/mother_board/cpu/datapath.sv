61896ce0f74bbe2275ed9b0c7298177dd6a0323e
Yuna Tomida <ytomida.mmm@gmail.com>
2021-11-29 19:29:57 +0900

    decoder の移動、命令/データレジスタの記述など

--- harris/multi/main/mother_board/cpu/datapath.sv
+++ harris/multi/main/mother_board/cpu/datapath.sv
@@ -1,24 +1,35 @@
+`include "lib_cpu.svh"
 module datapath(
     ctrl_bus_if.central ctrl_bus,
-    mem_bus_if.central  imem_bus,
-    mem_bus_if.central  dmem_bus,
+    mem_bus_if.central  mem_bus,
+	output	OPECODE		op,
+	output	FUNCT		funct,
     output  logic       zero,
     input   logic       mem_to_reg,
     input   logic       pc_src, alu_src,
     input   logic       reg_dst, reg_write,
     input   logic       jmp,
+	input	logic		ireg_write_enab,
+	input	logic		i_or_d,
     input   logic[2:0]  alu_ctrl_sig,
     output  logic[31:0] write_data
 );
-    logic[31:0] alu_out, inst, imm, reg_in, read_data;
+    logic[31:0] alu_out, inst, imm, reg_in, read_data, load_data;
     logic[31:0] pc, pc_plus4, pc_next, pc_br, pc_br_next, br_offset, pc_jmp;
     logic[31:0] alu_in1, alu_in2;
     logic[4:0]  reg_id;
 
-    assign imem_bus.addr = pc;
-    assign inst = imem_bus.data;
-    assign dmem_bus.addr = alu_out;
-    assign read_data = dmem_bus.data;
+	mux2 #(.N(32))	sel_addr(.sel(i_or_d), .src1(pc), .src2(alu_out), .out(mem_bus.addr))
+
+	// メモリから読んできたデータを命令/データレジスタに格納
+	enab_ff #(.N(32))	ireg(.ctrl_bus, .enab(ireg_write_enab), .in(mem_bus.data), .out(inst))
+	ff		#(.N(32))	dreg(.ctrl_bus, .in(mem_bus.data), .out(read_data));
+    
+    decoder decoder(
+        .prefix(inst[31:26]),
+        .suffix(inst[5:0]),
+        .op, .funct
+    );
 
 	// PC 制御
 	ff #(.N(32))	pcreg(.ctrl_bus, .in(pc_next), .out(pc));
